22953a60e62c963389e2d99e15eb76e05b0a9a7e
Yellowpig-zp <zipeng.qin.1997@outlook.com>
2018-07-21 00:14:30 -0700

    finished

--- safe_push.py
+++ safe_push.py
@@ -1,7 +1,25 @@
 #!/usr/bin python3
+import os
 
 with open("mysite/settings.py", "r") as f:
 	data = f.readlines()
 	print(data[1])
-my_secret_key = data[1].index("=")
-data[1] = "my_secret_key = 'nf922*1)4z4=k4rt!n9smgj6116@a(e3yjjm6d(5jc7b-1&_q4'"
+my_secret_key = data[1][data[1].index("=")+2:]
+data[1] = "my_secret_key = 'no key'"
+
+with open("mysite/settings.py", "w") as f:
+	f.writelines(data)
+
+commit_message = input("Enter commit message:")
+if not commit_message:
+	raise RuntimeError("Empty commit message as not allowed!")
+
+os.system("git add .")
+os.system("git commit -m %s" % commit_message)
+os.system("git push origin master")
+
+with open("mysite/settings.py", "r+") as f:
+	data = f.readlines()
+	data[1] = "my_secret_key = %s" % my_secret_key
+	f.writelines(data)
+
