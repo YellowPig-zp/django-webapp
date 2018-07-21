a360805fc338637fb23dd50124463ad690d07279
Yellowpig-zp <zipeng.qin.1997@outlook.com>
2018-07-21 00:22:43 -0700

    fixed commit message bug for safe_push.py

--- safe_push.py
+++ safe_push.py
@@ -5,7 +5,7 @@ with open("mysite/settings.py", "r") as f:
 	data = f.readlines()
 	print(data[1])
 my_secret_key = data[1][data[1].index("=")+2:]
-data[1] = "my_secret_key = 'no key'"
+data[1] = "my_secret_key = 'no key'\n"
 
 with open("mysite/settings.py", "w") as f:
 	f.writelines(data)
@@ -15,16 +15,13 @@ if not commit_message:
 	raise RuntimeError("Empty commit message as not allowed!")
 
 os.system("git add .")
-os.system("git commit -m %s" % commit_message)
+os.system("git commit -m \"%s\"" % commit_message)
 os.system("git push origin master")
 
-"""
+
 with open("mysite/settings.py", "r") as f:
 	data = f.readlines()
-
-data[1] = "my_secret_key = %s" % my_secret_key
-
-with open("mysite/settings.py", "w") as f:
+	data[1] = "my_secret_key = %s\n" % my_secret_key
 	f.writelines(data)
-"""
+
 
